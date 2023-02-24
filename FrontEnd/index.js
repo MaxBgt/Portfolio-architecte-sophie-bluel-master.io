54e4d57707d8aed1acd3cf6760402b6e8bc9b4f7
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-02-24 11:41:38 +0100

    implémentation des modales + ajout et suppression de projet

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,5 +1,24 @@
 const gallery = document.querySelector(".gallery");
 const btnFilter = document.querySelectorAll(".btn_filter");
+const modal = document.querySelector(".modal");
+const openModalBtn = document.querySelector(".btn_open");
+const closeModalBtn = document.querySelector(".close_modal");
+const overlay = document.querySelector(".overlay");
+const overlay_add_pic = document.querySelector(".overlay_add_pic");
+const pic_container = document.querySelector(".pic_container");
+const modal_pic = document.querySelectorAll(".modal_pic");
+const delete_btn = document.querySelectorAll(".delete_btn");
+const modal_add_pic = document.querySelector(".modal_add_pic");
+const add_pic_btn = document.querySelector(".add_pic");
+const add_valid = document.getElementById("add_valid");
+const form = document.getElementById("add_pic_form");
+const back_btn = document.querySelector(".fa-arrow-left");
+const closeAddModalBtn = document.querySelector(".close_add_modal");
+const inputFile = document.querySelector(".input-file");
+const labelFile = document.querySelector(".label-file");
+const upload_image = document.getElementById("upload_image");
+let uploadedImage = "";
+
 let projects = [];
 let filterMethod = "Tous";
 
@@ -46,4 +65,112 @@ btnFilter.forEach((btnFilter) => {
     filterMethod = e.target.id;
     projectsDisplay();
   });
+});
+
+const openModal = (e) => {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const openAddPicModal = (e) => {
+  e.preventDefault();
+  modal_add_pic.classList.remove("hidden_modal2");
+  overlay_add_pic.classList.remove("hidden_modal2");
+};
+const closeModal = (e) => {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const closeAddModal = (e) => {
+  modal_add_pic.classList.add("hidden_modal2");
+  overlay_add_pic.classList.add("hidden_modal2");
+};
+document.addEventListener("keydown", (e) => {
+  if (
+    e.key === "Escape" ||
+    (e.key === "Esc" && modal.classList.contains("hidden"))
+  ) {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+});
+
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+add_pic_btn.addEventListener("click", openAddPicModal);
+overlay_add_pic.addEventListener("click", closeAddModal);
+overlay.addEventListener("click", closeModal);
+back_btn.addEventListener("click", closeAddModal);
+closeAddModalBtn.addEventListener("click", closeAddModal);
+
+// Affichage des projets dans la modale
+
+const picDisplay = () => {
+  pic_container.innerHTML += projects
+    .map(
+      (project) =>
+        `
+          <div class="pic" id="${project.id}">
+          <img class="modal_pic" src="${project.imageUrl}" alt="${project.title}" id="${project.id}">
+          <button class="delete_btn"><i class="fa-regular fa-trash-can btn_delete" id="${project.id}"></i></button>
+          <p class="edit_text">éditer</p>
+          </div>
+          `
+    )
+    .join("");
+};
+// Supprimer projet dans la modale
+const fetchDelete = async (id) => {
+  await fetch(`http://localhost:5678/api/works/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("Bearer")}`,
+    },
+  })
+    .then((res) => res)
+    .then((res) => console.log(res));
+};
+
+pic_container.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchDelete(e.target.id);
+});
+
+// Fonction ajouter projet
+
+inputFile.addEventListener("change", (e) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploadedImage = reader.result;
+    document.getElementById(
+      "upload_image"
+    ).style.backgroundImage = `url(${uploadedImage})`;
+  });
+  reader.readAsDataURL(e.target.files[0]);
+  upload_image.style.zIndex = 1;
+});
+
+const fecthAdd = async () => {
+  let formData = new FormData(form);
+  for (item in formData) {
+    item[0], item[1];
+  }
+  await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("Bearer")}`,
+    },
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fecthAdd();
+  return false;
 });
